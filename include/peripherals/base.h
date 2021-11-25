a1ee49466985e77e5423776ad84ec626abfee4d6
IfanTsai <i@caiyifan.cn>
2021-11-25 22:02:44 +0800

    implement local timer and timed callback

--- include/peripherals/base.h
+++ include/peripherals/base.h
@@ -3,7 +3,9 @@
 
 #include "mm.h"
 
-#define DEVICE_BASE      0x3f000000
-#define PBASE            (VA_START + DEVICE_BASE)
+#define DEVICE_PHY_BASE      0x3f000000
+#define PERIPHERAL_PHY_BASE  0x40000000
+#define PBASE                (VA_START + DEVICE_PHY_BASE)
+#define PERIPHERAL_BASE      (VA_START + PERIPHERAL_PHY_BASE)
 
 #endif  // !__P_BASE_H__
--- irq/irq.c
+++ irq/irq.c
@@ -1,4 +1,5 @@
 #include "peripherals/irq.h"
+#include "peripherals/timer.h"
 #include "irq.h"
 #include "printf.h"
 #include "utils.h"
@@ -38,18 +39,34 @@ void show_invalid_entry_message(int type, unsigned long esr, unsigned long addre
 void enable_interrupt_controller()
 {
     put32(ENABLE_IRQS_1, SYSTEM_TIMER_IRQ_1);
+    put32(TIMER_CTRL, get32(TIMER_CTRL) | ( 1 << 29 ));
 }
 
 void handle_irq()
 {
-    unsigned int irq = get32(IRQ_PENDING_1);
+    unsigned int irq = get32(CORE0_INT_SOURCE);
+    irq &= (~GPU_IRQ);    // do not care GPU interrupt
+    if (irq) {
+        switch (irq) {
+        case LOCAL_TIME_IRQ:
+            handle_local_timer_irq();
+
+            break;
+        default:
+            printf("Unknown core 0 irq: 0x%x\r\n", irq);
+        }
+    }
+
+    irq = get32(IRQ_PENDING_1);
+    if (irq) {
 
     switch (irq) {
-    case (SYSTEM_TIMER_IRQ_1):
-        handle_timer_irq();
+        case (SYSTEM_TIMER_IRQ_1):
+            handle_sys_timer_irq();
 
-        break;
-    default:
-        printf("Unknown pending irq: %x\r\n", irq);
+            break;
+        default:
+            printf("Unknown pending 1 irq: 0x%x\r\n", irq);
+        }
     }
 }
